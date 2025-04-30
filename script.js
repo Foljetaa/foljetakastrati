fe7fa2f15455d1f5c10e85e638d2182762f253c5
Foljetaa <foljetakastrati@gmail.com>
2025-05-01 00:12:55 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $("section").hover(function () {
+    $(this).css("background-color", "#f9f9f9");
+  }, function () {
+    $(this).css("background-color", "#fff");
+  });
+});
